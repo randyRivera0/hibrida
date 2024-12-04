d1f304f1eba8e741ac52be101ac67649664d5c7e
randyRivera0 <rariberm@espol.edu.ec>
2024-12-04 14:56:29 -0500

    first

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { 
+  /* 1. Importe los componentes */
+  IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent,
+} from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent,
+],
 })
 export class Tab3Page {
   constructor() {}
